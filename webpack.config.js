f4e7ad9a52715fc6b7bbc37ac1687a970e098593
masaki <tomotsuka@thisco.jp>
2017-10-23 19:39:19 +0900

    ecosystem change

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+module.exports = {
+	entry: __dirname + "/src/main.js",
+	output:{
+		path:__dirname + '/dist',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			//loader
+		]
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, 'dist')
+	},
+};
