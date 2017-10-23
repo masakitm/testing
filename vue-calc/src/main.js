8b4850686e7604dd2d03ffe1e40ad39e0fd879aa
masaki <tomotsuka@thisco.jp>
2017-10-23 18:16:32 +0900

    calc v0.02

--- vue-calc/src/main.js
+++ vue-calc/src/main.js
@@ -3,20 +3,134 @@ import Vue from 'vue/dist/vue.js';
 var app = new Vue({
 	el:"#app",
 	data:{
-		buttons:["1","2","3","4","5","6","7","8","9","0",".","+","-","*","/"],
+		buttons:[
+			{
+				value:"C",
+				is:"func",
+				callback:"clearFormula"
+			},
+			{
+				value:"AC",
+				is:"func",
+				callback:"clearFormula"
+			},
+			{
+				value:"",
+				is:"func",
+				callback:""
+			},
+			{
+				value:"BACK",
+				is:"func",
+				callback:"back",
+			},
+			{
+				value:"7",
+				is:"num"
+			},
+			{
+				value:"8",
+				is:"num"
+			},
+			{
+				value:"9",
+				is:"num"
+			},
+			{
+				value:"/",
+				is:"math"
+			},
+			{
+				value:"4",
+				is:"num"
+			},
+			{
+				value:"5",
+				is:"num"
+			},
+			{
+				value:"6",
+				is:"num"
+			},
+			{
+				value:"*",
+				is:"math"
+			},
+			{
+				value:"1",
+				is:"num"
+			},
+			{
+				value:"2",
+				is:"num"
+			},
+			{
+				value:"3",
+				is:"num"
+			},
+			{
+				value:"-",
+				is:"math"
+			},
+			{
+				value:"0",
+				is:"num"
+			},
+			{
+				value:"00",
+				is:"num"
+			},
+			{
+				value:".",
+				is:"num"
+			},
+			{
+				value:"+",
+				is:"math"
+			},
+			{
+				value:"=",
+				is:"func",
+				callback:"runCalc",
+				class:"w100p"
+			}
+		],
 		formula:"",
-		pushedBtn:""
+		beforePushed:"num"
 	},
 	methods:{
-		getBtn:function(value){
-			this.pushedBtn = value;
-			this.formula = this.formula + value;
+		getBtn:function(value,is,callback){
+			if( is == "num" ){
+				this.formula = this.formula + value;
+
+			} else if( is == "math" ){
+				if(this.beforePushed == "math"){
+					this.formula = this.formula.slice( 0, -1 ) + value;
+				} else{
+					this.formula = this.formula + value;
+				}
+
+			} else if( is == "func" ){
+				switch (callback){
+					case "clearFormula":
+						this.clearFormula();
+						break;
+					case "back":
+						this.back();
+						break;
+					case "runCalc":
+						this.runCalc();
+						break;
+				}
+			}
+
+			this.beforePushed = is;
 		},
 		clearFormula:function(){
 			this.formula = "";
 		},
 		back:function(){
-			this.formula = this.formula.slice( 0, -1 );
+			this.formula = String(this.formula).slice( 0, -1 );
 		},
 		runCalc:function(){
 			this.formula = eval(this.formula);
