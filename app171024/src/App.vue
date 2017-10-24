fec0bc804bef3de53b74f4728858bf6e09608a50
masaki <tomotsuka@thisco.jp>
2017-10-24 15:32:45 +0900

    single component

---
+++ app171024/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+		<textarea name="name" rows="8" cols="80" v-model="inputTxt"></textarea>
+		<div class="">
+			<div class="">
+				{{ this.inputTxt }}
+			</div>
+
+			<div class="" v-if="this.inputTxt">
+				{{ this.reverseStr() }}
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      inputTxt: ''
+    }
+  },
+  methods: {
+    reverseStr: function () {
+      var str = this.inputTxt
+      return str.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
