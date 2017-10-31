8a785166f04dc7044c9ce0585262720e1e0010ce
masaki <tomotsuka@thisco.jp>
2017-10-31 21:30:01 +0900

    after mokumoku 171031

---
+++ vuex01/src/components/modules/TextareaComp.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p class="error">{{ error }}</p>
+    <textarea v-model="impression"></textarea>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'textarea',
+  computed: {
+    impression: {
+      get () {
+        return this.$store.state.imporession
+      },
+      set (value) {
+        this.$store.commit('updateImpression', value)
+      }
+    },
+    ...mapGetters('Textarea', {
+      'error': 'getError'
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.error{
+  color: red;
+}
+</style>
